49a9e54f4d7aacbf2cb162babe6ead7c150502b5
Tsunghao <tsunghao.chen12@gmail.com>
2025-03-09 18:43:33 +0100

    ex01 complete

--- DS_3_DataScientist_1/ex01/Correlation.py
+++ DS_3_DataScientist_1/ex01/Correlation.py
@@ -3,24 +3,36 @@ import pandas as pd
 import scipy.stats
 
 
+def corr_generate(df: pd.DataFrame, target: str) -> dict:
+    """
+    return a dictionary {feature_name: (corr, p_value)} to target column
+    """
+    results = {}
+    for feature in df.columns:
+        corr, p_value = scipy.stats.pearsonr(df[feature], df[target])
+        results[feature] = (abs(corr), p_value)
+    
+    # sort by absolute correlation value (strongest to weakest)
+    sorted_result = dict(sorted(results.items(), key=lambda x: abs(x[1][0]), reverse=True))
+    return sorted_result
+
+
 def main():
     try:
         df = pd.read_csv("Train_knight.csv")
-        # replace string with 0 and 1, and then replace the column
-        df['target'] = df['knight'].apply(lambda x: 1 if x == 'Jedi' else 0)
-        df.drop(['knight'], axis=1, inplace=True)
-        df.rename(columns={'target':'knight'}, inplace=True)
+
+        # 1: replace string with 0 and 1, and then replace the column
+        # df['target'] = df['knight'].apply(lambda x: 1 if x == 'Jedi' else 0)
+        # df.drop(['knight'], axis=1, inplace=True)
+        # df.rename(columns={'target':'knight'}, inplace=True)
+        df['knight'] = df['knight'].astype('category').cat.codes
         # print(df)
 
-        # iterate and print all correlation coefficient
-        correlations = []
-        for column in df.columns:
-            correlations.append(scipy.stats.pearsonr(df[column], df['knight'])[0])
-        # print(correlations)
-        df_corr = pd.DataFrame(zip(df.columns, correlations), columns=['feature', 'corr'])
-        df_corr.set_index('feature', inplace=True)
-        print(df_corr.sort_values(by='corr', ascending=False))
-            
+        pearson_corr = corr_generate(df, 'knight')
+        # print(pearson_corr)
+        for feature, (corr, p_value) in pearson_corr.items():
+            print(f"{feature:<15}{corr:.6f}")
+
     except Exception as e:
         print("Error", e)
 
