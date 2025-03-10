a2b15a320e3239d16a46fbe45ab6d0686c032156
Tsunghao <tsunghao.chen12@gmail.com>
2025-03-10 09:03:04 +0100

    ex03 start

---
+++ DS_3_DataScientist_1/ex03/standardization.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.preprocessing as prep
+
+
+def main():
+    try:
+        pass
+    except Exception as e:
+        print("Error", e)
+
+
+if __name__ == "__main__":
+    main()
